245f704a502eb80eae8287889c8ecefc74a6ec4d
Eshitacodes <113260555+Eshitacodes@users.noreply.github.com>
2023-09-10 20:15:26 +0530

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import javax.swing.JFrame;
+class Frame
+{
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setVisible(true);
+    }
+}
